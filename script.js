9a056d4fcc0fc947be15030ca12c65112d4bd93a
PrajanBasnet <spiderbasnet140@gmail.com>
2024-11-17 23:32:55 +0545

    all done

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const canvas = document.getElementById("gameFrame");
 const ctx = canvas.getContext("2d");
-let speedX = 13;
-let speedY = 13;
+
+
+// global variables for snake
+let speedX = 20;
+let speedY = 20;
 let snakeX = Math.floor(Math.random() * 100);
 let snakeY = Math.floor(Math.random() * 300);
 let width = 20;
@@ -10,17 +13,18 @@ let foodX = Math.floor(Math.random() * 380);
 let foodY = Math.floor(Math.random() * 380);
 let score = 0;
 let count = 0;
+let start = 0;
 
-window.onload = function () {
-    // setInterval(food,1000/1);
-    
+
+// this function let's you start the game
+function Start(){
     setInterval(food , 1000/3);
     setInterval(snake, 1000 / 3);
     setInterval(SnakeBorder,1000/3);
     
+    
 }
-
-
+// function for food update and score
 function food() {
     ctx.fillStyle = "blue";
     ctx.fillRect(foodX, foodY, 10, 10);
@@ -31,19 +35,22 @@ function food() {
         foodY = Math.floor(Math.random() * 380);
         width += 8;
         height += 8;
+        score += 1;
         
     }
+    document.getElementById("score").innerHTML = "Score -" + score;
 }
 
+// for game reset if dead
 function SnakeBorder(){
-    if(snakeX > 460 || snakeX < 1  || snakeY <0 || snakeY >460 ){
+    if(snakeX > 460 || snakeX < 0  || snakeY <0 || snakeY >460 ){
         console.log("you ran on border");
         alert("Game over");
         window.location.reload();
     }
 }
 
-
+//  all the control using keyboard 
 function snake() {
     ctx.fillStyle = "red";
     ctx.clearRect(snakeX, snakeY, width, height);
@@ -71,25 +78,21 @@ function changeDirection() {
     document.addEventListener("keyup", (event) => {
         if (event.key == "ArrowDown" && count != -1) {
             ctx.clearRect(snakeX, snakeY, width, height);
-            snakeY += speedY;
             ctx.fillRect(snakeX, snakeY, width, height);
             count = 1;
         } else if (event.key == "ArrowUp" && count != 1) {
             ctx.clearRect(snakeX, snakeY, width, height);
-            snakeY -= speedY;
             ctx.fillRect(snakeX, snakeY, width, height);
             count = -1;
 
         }
         else if (event.key == "ArrowRight" && count != -2) {
             ctx.clearRect(snakeX, snakeY, width, height);
-            snakeX += speedX;
             ctx.fillRect(snakeX, snakeY, width, height);
             count = 2;
         }
         else if (event.key == "ArrowLeft" && count != 2) {
             ctx.clearRect(snakeX, snakeY, width, height);
-            snakeX -= speedX;
             ctx.fillRect(snakeX, snakeY, width, height);
             count = -2;
         }
